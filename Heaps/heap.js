58440e4e97f2abcea9063abdbf2f431179591240
Chris <chrisqcs@tuta.io>
2026-02-19 16:28:22 -0300

    Heap Building, recursive and looped

--- Heaps/heap.js
+++ Heaps/heap.js
@@ -5,34 +5,27 @@ export class Heap extends ArrayBT {
   #max
 
   constructor(min, array = []) {
+    if(typeof min !== 'boolean') throw "Invalid min/max type"
+
     super(array)
     this.#min = min
     this.#max = !min
 
-    this.#constructHeapFromOriginalTree()
+    this.#constructHeapFromOriginalTreeRecursive()
+    // this.#constructHeapFromOriginalTree()
   }
 
-  // Ends up being ~O(n) since subtrees get
-  // progressively more ordered; leaf node populace
-  // get no attention; and each iteration
-  // runs at most H heapifyT-B recursions
   #constructHeapFromOriginalTree() {
     const heapLen = this.tree.length
-    for(let i = heapLen - 1; i >= 0; i--) {
+    for(let i = this.parentIndexFrom(heapLen-1); i >= 0; i--) {
       this.#heapifyTopBottom(i)
     }
   }
 
-  peek() {
-    return this.tree[0]
-  }
-
-  push(element) {
-    // add to the bottom
-    const newLength = this.tree.push(element)
-    // heapify from the element's position
-    this.#heapifyBottomTop(newLength - 1)
-  }
+  // Ends up being ~O(n) since subtrees get
+  // progressively more ordered; leaf node populace
+  // are ultimately ignored; and each iteration
+  // runs at most H heapifyT-B recursions
 
   #heapifyBottomTop(startingPosition = this.tree.length - 1) {
     const parentIndex = this.parentIndexFrom(startingPosition)
@@ -55,55 +48,86 @@ export class Heap extends ArrayBT {
     this.#heapifyBottomTop(parentIndex)
   }
 
+  #heapifyTopBottom(startingPosition = 0) {
+    while(true) {
+      const leftIndex = this.leftIndexFrom(startingPosition);
+      const rightIndex = this.rightIndexFrom(startingPosition)
+
+      const leftValue = this.tree[leftIndex]
+      const rightValue = this.tree[rightIndex]
+      let positionToChange = startingPosition;
+
+      // #min and #max kept mutually exclusive, safe to use || later
+      const leftMinCondition = this.#min && leftValue < this.tree[positionToChange] && leftIndex < this.size()
+      const leftMaxCondition = this.#max && leftValue > this.tree[positionToChange] && leftIndex < this.size()
+      if(leftMinCondition || leftMaxCondition) positionToChange = leftIndex
+      const rightMinCondition = this.#min && rightValue < this.tree[positionToChange] && rightIndex < this.size()
+      const rightMaxCondition = this.#max && rightValue > this.tree[positionToChange] && rightIndex < this.size()
+      if (rightMinCondition || rightMaxCondition) positionToChange = rightIndex
+
+      if (startingPosition === positionToChange) break;
+
+      // Swap
+      const buffer = this.tree[positionToChange]
+      this.tree[positionToChange] = this.tree[startingPosition]
+      this.tree[startingPosition] = buffer
+
+      startingPosition = positionToChange
+    }
+  }
+
+  #constructHeapFromOriginalTreeRecursive() {
+    const heapLen = this.tree.length
+    for(let i = this.parentIndexFrom(heapLen-1); i >= 0; i--) {
+      this.#heapifyTopBottomRecursive(i)
+    }
+  }
+  #heapifyTopBottomRecursive(startingPosition) {
+    const leftIndex = this.leftIndexFrom(startingPosition)
+    const rightIndex = this.rightIndexFrom(startingPosition)
+
+    const leftValue = this.tree[leftIndex]
+    const rightValue = this.tree[rightIndex]
+    let positionToChange = startingPosition
+
+    const minLeftCondition = this.#min && leftValue < this.tree[positionToChange];
+    const maxLeftCondition = this.#max && leftValue > this.tree[positionToChange];
+    if(minLeftCondition || maxLeftCondition) positionToChange = leftIndex
+
+    const minRightCondition = this.#min && rightValue < this.tree[positionToChange];
+    const maxRightCondition = this.#max && rightValue > this.tree[positionToChange];
+    if(minRightCondition || maxRightCondition) positionToChange = rightIndex
+
+    if(positionToChange !== startingPosition) {
+      const buffer = this.tree[positionToChange]
+      this.tree[positionToChange] = this.tree[startingPosition]
+      this.tree[startingPosition] = buffer
+    } else {
+      return;
+    }
+
+    this.#heapifyTopBottomRecursive(positionToChange)
+  }
+
+  peek() {
+    return this.tree[0]
+  }
+
   pop() {
     const poppingTopValue = this.peek();
     this.tree[0] = this.tree[this.size() - 1];
     this.tree.pop();
 
-    this.#heapifyTopBottom(0)
+    this.#heapifyTopBottomRecursive(0)
 
     return poppingTopValue;
   }
 
-  #heapifyTopBottom(startingPosition) {
-    const leftIndex = this.leftIndexFrom(startingPosition)
-    const rightIndex = this.rightIndexFrom(startingPosition)
-    let nextPosition;
-
-    const leftValue = this.tree[leftIndex]
-    const rightValue = this.tree[rightIndex]
-    const positionValue = this.tree[startingPosition]
-
-    const evalSwap = (direction) => {
-      let minCondition;
-      let maxCondition;
-      if(direction === 'left') {
-        if(leftIndex >= this.size()) return false
-        minCondition = this.#min && leftValue < positionValue;
-        maxCondition = this.#max && leftValue > positionValue;
-      } else if(direction === 'right') {
-        if(rightIndex >= this.size()) return false
-        minCondition = this.#min && rightValue < positionValue;
-        maxCondition = this.#max && rightValue > positionValue;
-      } else {
-        throw new Error("Invalid evalSwap direction")
-      }
-      return minCondition || maxCondition;
-    }
-
-    if(evalSwap('left')) {
-      this.tree[startingPosition] = leftValue;
-      this.tree[leftIndex] = positionValue;
-      nextPosition = leftIndex;
-    } else if(evalSwap('right')) {
-      this.tree[startingPosition] = rightValue;
-      this.tree[rightIndex] = positionValue;
-      nextPosition = rightIndex
-    } else {
-      return;
-    }
-
-    this.#heapifyTopBottom(nextPosition)
+  push(element) {
+    // add to the bottom
+    const newLength = this.tree.push(element)
+    // heapify from the element's position
+    this.#heapifyBottomTop(newLength - 1)
   }
 }
 
@@ -134,6 +158,11 @@ function runHeaps() {
 
   const minConstruction = new Heap(true, [8,7,6,5,4,3,2,1,0])
   console.log(minConstruction.traverseLevelOrder())
+
+  console.log('\n', "********** MAX HEAP CONSTRUCTION **********", '\n')
+
+  const maxConstruction = new Heap(false, [8,7,6,5,4,3,2,1,0].reverse())
+  console.log(maxConstruction.traverseLevelOrder())
 }
 
 runHeaps()
