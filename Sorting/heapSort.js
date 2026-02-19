fcf4fc1ad53f1d24486c2680656dec511ff74788
Chris <chrisqcs@tuta.io>
2026-02-19 16:42:42 -0300

    Heap Sort, Descending Order

--- Sorting/heapSort.js
+++ Sorting/heapSort.js
@@ -3,8 +3,8 @@ import {Heap} from "../Heaps/heap.js";
 class HeapSort {
   #heap
 
-  constructor(arrayToSort) {
-    this.#heap = new Heap(true, arrayToSort)
+  constructor(arrayToSort, ascending = true) {
+    this.#heap = new Heap(ascending, arrayToSort)
   }
 
   sorted() {
@@ -18,6 +18,7 @@ class HeapSort {
 }
 
 function runHeapSort() {
+  console.log("************ ASCENDING ORDER HEAP SORT ************")
   const unorderedRandomArray = [23456,76,78,678,8,26,315,12,4,465,247,568,6,8956457,5342,634,513,453,46542,75,85,78];
   const randomHeapSort = new HeapSort(unorderedRandomArray);
   console.log(randomHeapSort.sorted())
@@ -25,6 +26,15 @@ function runHeapSort() {
   const worstCaseSimple = [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
   const simpleHeapSort = new HeapSort(worstCaseSimple);
   console.log(simpleHeapSort.sorted())
+
+  console.log("************ DESCENDING ORDER HEAP SORT ************")
+  const unorderedRandomArrayDesc = [23456,76,78,678,8,26,315,12,4,465,247,568,6,8956457,5342,634,513,453,46542,75,85,78];
+  const randomHeapSortDesc = new HeapSort(unorderedRandomArrayDesc, false);
+  console.log(randomHeapSortDesc.sorted())
+
+  const worstCaseSimpleDesc = [20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+  const simpleHeapSortDesc = new HeapSort(worstCaseSimpleDesc,false);
+  console.log(simpleHeapSortDesc.sorted())
 }
 
 runHeapSort()
